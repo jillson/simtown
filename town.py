d5adb582f018db37c16125778c36bca750a17eb4
carleton <carletronicon@gmail.com>
2016-01-18 20:35:48 -0500

    moved utility functions from town into a utility file; updated to make tests work cleaner (mainly handle looking up an attribute that isn't set returns default 0 versus throwing exception

--- town.py
+++ town.py
@@ -1,35 +1,9 @@
-from collections import defaultdict
-from operator import mul,add
-#from functools import reduce #uncomment for python3
 import random
 
+from utils import pickBest, makeRule, makeWeightedRule, makeWeightedAddRule
 from people import Person,reaper
 
 
-def pickBest(alist,metric,n=1,picker=None):
-    if len(alist) <= n:
-        return alist
-    mdict = defaultdict(list)
-    rez = []
-    for p in alist:
-        mdict[metric(p,picker)].append(p)
-    while(len(rez) < n):
-        bestMetric = max(mdict.keys())
-        foo = mdict[bestMetric]
-        random.shuffle(foo)
-        rez += foo
-        mdict.pop(bestMetric)
-    return rez[:n]
-
-#TODO: make this be handle p
-def makeRule(attribs):
-    return lambda x,p=None: reduce(mul,[x.__dict__(a) for a in attribs])
-
-def makeWeightedRule(weighted_attribs):
-    return lambda x,p=None: reduce(mul,[x.__dict__[a[1]]*a[0] for a in weighted_attribs])
-
-def makeWeightedAddRule(weighted_attribs):
-    return lambda x,p=None: reduce(add,[x.__dict__[a[1]]*a[0] for a in weighted_attribs])
 
 class Town(object):
     def __init__(self,name):
---
+++ utils.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from operator import mul,add
+#from functools import reduce #uncomment for python3
+import random
+
+def pickBest(alist,metric,n=1,picker=None):
+    if len(alist) <= n:
+        if len(alist) > 0:
+            random.shuffle(alist)
+        return alist
+    mdict = defaultdict(list)
+    rez = []
+    for p in alist:
+        mdict[metric(p,picker)].append(p)
+    while(len(rez) < n):
+        bestMetric = max(mdict.keys())
+        foo = mdict[bestMetric]
+        random.shuffle(foo)
+        rez += foo
+        mdict.pop(bestMetric)
+    return rez[:n]
+
+#TODO: make this be handle p
+def makeRule(attribs):
+    return lambda x,p=None: reduce(mul,[x.__dict__.get(a,0) for a in attribs])
+
+def makeWeightedRule(weighted_attribs):
+    return lambda x,p=None: reduce(mul,[x.__dict__.get(a[1],0)*a[0] for a in weighted_attribs])
+
+def makeWeightedAddRule(weighted_attribs):
+    return lambda x,p=None: reduce(add,[x.__dict__.get(a[1],0)*a[0] for a in weighted_attribs])
